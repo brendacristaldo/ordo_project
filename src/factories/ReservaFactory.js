a57a79436a67240d9f7604ee81d9fac5492758d1
Brenda Cristaldo <112977198+brendacristaldo@users.noreply.github.com>
2025-07-04 20:51:26 -0300

    update

--- src/factories/ReservaFactory.js
+++ src/factories/ReservaFactory.js
@@ -1,15 +1,14 @@
-// src/factories/ReservaFactory.js
 const Reserva = require('../entities/Reserva');
 
 class ReservaFactory {
     static create(data) {
-        // 1. Validação dos dados brutos
+        // 1. validação dos dados brutos
         if (!data.titulo || !data.sala_id || !data.usuario_id || !data.data_inicio || !data.data_fim) {
             throw new Error('Dados insuficientes para criar uma reserva. Título, sala, usuário e datas são obrigatórios.');
         }
 
-        // 2. Cria e retorna a instância da entidade Reserva
-        // Os dados aqui já estão validados e prontos para se tornarem um objeto de domínio.
+        // 2. aqui e retorna a instância da entidade Reserva
+        // os dados aqui já estão validados e prontos para se tornarem um objeto de domínio.
         return new Reserva(data);
     }
 }
